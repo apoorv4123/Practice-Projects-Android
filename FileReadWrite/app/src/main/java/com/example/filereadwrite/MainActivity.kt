dcdc2374c71b6eb2439b89d8c26e8f16995d499a
Apoorv Srivastava <apoorvsrivastava.4123@gmail.com>
2021-04-19 22:38:59 +0530

    file read & write

---
+++ FileReadWrite/app/src/main/java/com/example/filereadwrite/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.filereadwrite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnWrite.setOnClickListener {
+            val dataDir = ContextCompat.getDataDir(this)
+            val myFile = File(dataDir,"file.txt")
+            val fos = FileOutputStream(myFile,true)
+            fos.write(etInput.text.toString().toByteArray())
+//            OR
+            myFile.writeText(etInput.text.toString())
+        }
+
+        btnRead.setOnClickListener {
+            val dataDir = ContextCompat.getDataDir(this)
+            val myFile = File(dataDir,"file.txt")
+            tvDisplay.text = myFile.readText()
+        }
+
+
+
+    }
+}
